94bbc994844bcdbcbd1487e88874af22c3ee50f4
ALREstevam <a166348@g.unicamp.com>
2018-02-15 17:16:53 -0200

    updsts

--- js/changeStylesheet.js
+++ js/changeStylesheet.js
@@ -7,3 +7,25 @@ function changeStylesheet(prefix, linkId) {
     // troca o valor de href num elemento <link> com id = linkId entre <prefixo>< 1 | 2 >.css
     document.getElementById(linkId).href = prefix + (cssCount + 1) + '.css';
 }
+
+var sheetList = [];
+var currentSheet = 0;
+
+function setSheetList(infoArr) {
+    sheetList = infoArr;
+}
+
+function calcNextElem(total, current) {
+    return (current + 1) % total;
+}
+
+
+function nextSheet(linkid) {
+    if(sheetList.length > 0){
+        currentSheet = calcNextElem(sheetList.length, currentSheet);
+        document.getElementById(linkid).href = sheetList[currentSheet];
+    }else{
+        console.log('No stylesheet was set to make the swap.')
+    }
+}
+
